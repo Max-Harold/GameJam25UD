ea9255e9ca07a41419c7284df323c64d0551da00
a2k6 <adrian@panezic.com>
2025-03-01 16:33:00 -0600

    Second Level

--- scenes/level_handler.gd
+++ scenes/level_handler.gd
@@ -8,10 +8,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	material.set_shader_parameter("lvl", Globals.lvl)
-	
+
+func win():
+	$CanvasLayer/GameOver/VBoxContainer/Game.text = "You win!"
+	$CanvasLayer/GameOver.visible = true
+	$CanvasLayer/GameOver/VBoxContainer/Restart.visible = false
 
 func game_over():
+	$CanvasLayer/GameOver/VBoxContainer/Game.text = "Game over"
 	$CanvasLayer/GameOver.visible = true
+	$CanvasLayer/GameOver/VBoxContainer/Restart.visible = true
 
 func _on_player_dies() -> void:
 	game_over()
