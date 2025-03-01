e6a1f8dd20310eeaefc3e5d9d7c25ffb81505b39
Max-Harold <maximilian.harold@gmail.com>
2025-03-01 16:00:57 -0600

    change to level 2

--- red.gd
+++ red.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
-		Globals.lvl=3
+		if Globals.lvl == 0:
+			get_tree().change_scene_to_file("res://scenes/levelTwo.tscn")
+			Globals.lvl=3
 		queue_free()
