9be08b94a964f4f33ac57b8cefd2fa74720886ad
Max-Harold <maximilian.harold@gmail.com>
2025-03-01 16:10:15 -0600

    wait a second

--- red.gd
+++ red.gd
@@ -3,6 +3,9 @@ extends Sprite2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		if Globals.lvl == 0:
-			get_tree().change_scene_to_file("res://scenes/levelTwo.tscn")
 			Globals.lvl=3
-		queue_free()
+			visible = false
+			await get_tree().create_timer(1.3).timeout
+			get_tree().change_scene_to_file("res://scenes/levelTwo.tscn")	
+			
+		
