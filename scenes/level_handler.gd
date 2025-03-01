bfec20fd16e7b82b6449b6c10c036964128e5fc1
Max-Harold <maximilian.harold@gmail.com>
2025-03-01 01:50:14 -0600

    simple level template

--- scenes/level_handler.gd
+++ scenes/level_handler.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+@onready var player = $Player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("handler")
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func game_over():
+	$CanvasLayer/GameOver.visible = true
+
+func _on_player_dies() -> void:
+	game_over()
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
